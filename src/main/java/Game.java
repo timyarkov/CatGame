292f5595a612a7a9d24165b17f519bf95b6d5595
Tim <timdyarkov@gmail.com>
2021-02-03 03:02:57 +1100

    worked on game engine part

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,9 @@
 package main.java;
 
+import main.java.application.MainApp;
 import main.java.GameStates;
+import main.java.Tickable;
+import main.java.GameObj;
 import main.java.gameobj.StaticObj;
 import main.java.gameobj.staticobj.Wall;
 import main.java.gameobj.staticobj.Spawn;
@@ -11,48 +14,94 @@ import main.java.gameobj.movingobj.LaserPointer;
 import main.java.gameobj.movingobj.Cat;
 import main.java.gameobj.movingobj.cat.Runny;
 import main.java.gameobj.movingobj.cat.Jumpy;
+
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.HashMap;
 import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Game {
     private GameStates state; //!!! Implement me after actual game is working!
 
-    // To be used in calculating board size
+    // To be used in calculating relative measurements
+    // Calculated to fit a GRIDSIZE * GRIDSIZE grid
+    private static final int GRIDSIZE = 300; //!!! need to test and experiment with this one
+    private static final int OBJSIZE = 30; //!!! this too
     private int deviceWidth;
     private int deviceHeight;
+    private float scaler; // How much to scale measurements by, given the resolution
 
     private Map<String, PImage> sprites; // Sprites to use in the game
 
-    // Game board components
-    private List<StaticObj> board;
-    private List<Wall> walls;
+    // Game board components; set up at initial setup
+    private List<StaticObj> board; //!!!dont need probably?
+    private List<Wall> walls; //!!!dont need probably?
     private PlayerSpawn playerSpawn;
     private List<CatSpawn> catSpawns;
     
-    // Gameplay components
+    // Gameplay components; newly made each game session, destroyed after
+    private List<GameObj> gameObjs;
     private List<MovingObj> entities;
     private LaserPointer player;
     private List<Cat> cats;
 
-    public Game(int deviceWidth, int deviceHeight, Map<String, PImage> sprites) {
+    public Game(MainApp app) {
         // Something must be reaaally wrong if this branch needs to be used
-        if (deviceWidth < 0) {
+        if (MainApp.SCREENWIDTH < 0) {
             this.deviceWidth = 480; // Lowest reasonable resolution I can think of 
-        } else if (deviceHeight < 0) {
+        } else if (MainApp.SCREENHEIGHT < 0) {
             this.deviceHeight = 640;
         }
 
-        this.deviceWidth = deviceWidth;
-        this.deviceHeight = deviceHeight;
+        this.deviceWidth = MainApp.SCREENWIDTH;
+        this.deviceHeight = MainApp.SCREENHEIGHT;
 
-        this.initialise(); // Initalise the game
+        this.initialise(app); // Initalise the game
     }
 
     // Setup Methods
-    public void initialise() {
-        // implement me!!!!
+    public void initialise(MainApp app) {
+        // Initialise all variables
+        // Calculate scaling factor
+        this.scaler = this.deviceWidth / 300; //!!! probably need to test, really unsure on the maths here
+
+        // State setup; start in title state
+        this.state = GameStates.Title;
+
+        this.sprites = new HashMap<String, PImage>();
+        // !!! When there are sprites, load them here via app.loadImage(...)
+        // !!! For now though, just leave as blank
+        this.sprites.put("Wall", new PImage());
+        this.sprites.put("Treat", new PImage());
+        this.sprites.put("LaserPointer", new PImage());
+        this.sprites.put("Runny", new PImage());
+        this.sprites.put("Jumpy", new PImage());
+        this.sprites.put("Stumbly", new PImage());
+        this.sprites.put("CatSpawn", new PImage());
+
+        //!!!!!!!!!!below is sooo messy, ill need to clean it up
+
+        // Wall setup; create boundaries
+        this.walls = new ArrayList<Wall>();
+        this.walls.add(new Wall(0, 0, this.deviceWidth, this.deviceHeight * 0.1f, 
+                            this.sprites.get("Wall"))); // Top bound (10% from top)
+        //!!! clean this multiline mess sometime
+        this.walls.add(new Wall(0, this.deviceHeight * 0.1f + this.scaler * Game.GRIDSIZE, this.deviceWidth, 
+                            this.deviceHeight - (this.deviceHeight * 0.1f + this.scaler * Game.GRIDSIZE), 
+                            this.sprites.get("Wall"))); // Bottom bound (rest after play area)
+        // Left bound (little bit showing)
+        this.walls.add(new Wall(-1, 0, 6, this.deviceHeight, this.sprites.get("Wall"))); 
+        // Right bound (little bit showing)
+        this.walls.add(new Wall(this.deviceWidth - 5, 0, 6, this.deviceHeight, this.sprites.get("Wall"))); 
+        
+        // Spawn setups; player spawn in middle of play area, and cat spawn around top
+        // Offsets of object widths to make spawned objects centered
+        this.playerSpawn = new PlayerSpawn((this.deviceWidth / 2) - Game.OBJSIZE, 
+                                                (this.deviceHeight * 0.1f) + (this.scaler * (Game.GRIDSIZE / 2f)) - Game.OBJSIZE,
+                                                Game.OBJSIZE, Game.OBJSIZE);
+        //Add catspawn(s)
     }
 
     // Getter Methods: General Game Attributes
@@ -66,6 +115,10 @@ public class Game {
 
     public int getDeviceHeight() {
         return this.deviceHeight;
+    }
+
+    public List<GameObj> getGameObjs() {
+        return this.gameObjs;
     }
 
     public Map<String, PImage> getSprites() {
@@ -100,5 +153,16 @@ public class Game {
 
     public List<Cat> getCats() {
         return this.cats;
+    }
+
+    // Game functionality
+    public void tick() {
+        // Run tick methods for each possible state
+        if (this.getState() == GameStates.InGame) {
+            // Run all tickables
+            for (Tickable obj : this.getGameObjs()) { //!!!need to edit this
+                obj.tick(this);
+            }
+        }
     }
 }
---
+++ src/main/java/application/MainApp.java
@@ -0,0 +1,7 @@
+package main.java.application;
+
+import processing.core.PApplet;
+public class MainApp extends PApplet {
+    public static final int SCREENWIDTH = 640; // Placeholder for now, get it to query device resolution
+    public static final int SCREENHEIGHT = 480;
+}
